59810bdcb510275feb466cf5c5912ff1c436ef88
Precious Echapare <preciousechapare@gmail.com>
2026-02-07 18:36:32 +0800

    Added section info for Echapare.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Lab 1: Version Control")
-print("Author: Precious Echapare")
+﻿print("Lab 1: Version Control")
+print("Author: Precious Echapare"
 print("Student ID: TUPM-25-3619")
+print("Class Section: [BSECE-1D]")
